e7823e4441f44d58ae14f8e13d196b35f3b33938
Haavaala <97886972+Haavaala@users.noreply.github.com>
2022-12-09 14:33:12 +0100

    Add files via upload

--- CourseProject-IDG2012/script.js
+++ CourseProject-IDG2012/script.js
@@ -1,48 +1,28 @@
-
-
-
     const button = document.querySelector(".dropbtn");
     const navbarLinks = document.querySelector(".dropdown-content");
     const Abutton = document.querySelector(".Idyll");
     const idyll = document.querySelector(".popupIdyll");
     const closePopup = document.querySelector(".close-popup");
-
-    const tpButton = document.querySelector(".tpButton");
-    const typiskNorsk = document.querySelector(".typiskNorsk");
-
-    const ccButton = document.querySelector(".CCbutton");
-    const kanskje = document.querySelector(".kanskje");
-
     const pizzaButton = document.querySelector(".pizza");
     const stektPizza = document.querySelector(".stektPizza");
 
-    const wigButton = document.querySelector(".wigButton");
-    const dreams = document.querySelector(".dreams")
 
+    //Open the navbar on smaller screens when pressing the menu button
     button.addEventListener("click", () =>{
         navbarLinks.classList.toggle("active");
     })
 
+    //Open the popup for the idyll
     Abutton?.addEventListener("click", () =>{
         idyll.classList.toggle("active");
     })
 
+    //Close the popup for the idyll
     closePopup?.addEventListener("click", () =>{
         idyll.classList.remove("active");
     })
 
-    tpButton.addEventListener("click", () =>{
-        typiskNorsk.classList.toggle("active");
-    })
-
-    ccButton.addEventListener("click", () =>{
-        kanskje.classList.toggle("active");
-    })
-
+    //Open the lyrics for the audio file
     pizzaButton.addEventListener("click", () =>{
         stektPizza.classList.toggle("active");
-    })
-
-    wigButton.addEventListener("click", () =>{
-        dreams.classList.toggle("active");
     })
